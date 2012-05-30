bd83f23f9d455a521872f099662955de71ba712a
Mike Slinn <mslinn@micronauticsresearch.com>
2012-05-30 00:00:11 -0700

    First cut at contacting AWS S3 from Heroku

--- src/main/java/servlet/HelloServlet.java
+++ src/main/java/servlet/HelloServlet.java
@@ -1,27 +1,25 @@
 package servlet;
 
-import java.io.IOException;
-
 import javax.servlet.ServletException;
 import javax.servlet.ServletOutputStream;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
 
 @WebServlet(
-        name = "MyServlet", 
+        name = "MyServlet",
         urlPatterns = {"/hello"}
+
     )
 public class HelloServlet extends HttpServlet {
 
     @Override
-    protected void doGet(HttpServletRequest req, HttpServletResponse resp)
-            throws ServletException, IOException {
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         ServletOutputStream out = resp.getOutputStream();
-        out.write("hello heroku".getBytes());
+        out.write("Hello, Heroku".getBytes());
         out.flush();
         out.close();
     }
-    
 }
--- src/main/webapp/index.jsp
+++ src/main/webapp/index.jsp
@@ -1,5 +1,11 @@
+<%@page import="com.micronautics.aws.S3" %>
 <html>
 <body>
-<h2>Hello Heroku!</h2>
-</body>
+<h2>Hello AWS from Heroku!</h2><%
+S3 s3 = new S3();
+if (s3.exception!=null) {
+  %>Exception: <%=s3.exception.getMessage()%><br/><%
+} else { %>
+  Your buckets are: <%=s3.listBuckets()%><br/><%
+} %></body>
 </html>
